230ee88a4d0239f3225e85167022e85bd3c2b71a
MarcioFelipe02 <marciofelipe02@outlook.com>
2024-05-21 02:06:18 -0300

    meu projeto conta banco

--- conta-banco/src/contaTerminal.java
+++ conta-banco/src/contaTerminal.java
@@ -15,7 +15,7 @@ public class contaTerminal {
         nomeCliente = teclado.nextLine();
         System.out.println("por favor digite o numero de sua conta");
         numeroConta = teclado.nextInt();
-        System.out.println("Olá" + nomeCliente + "Obrigado por abrir uma conta em nosso banco, sua agencia é"  + agencia + ", conta" + numeroConta + "e seu saldo é de" + saldo + "já esta disponível para saque" );
+        System.out.println("Olá " + nomeCliente + " Obrigado por abrir uma conta em nosso banco, sua agencia é "  + agencia + ", conta " + numeroConta + " e seu saldo é de " + saldo + " já esta disponível para saque" );
 
 
         
